7f7ddeb9ced5e8bb5e104896024739f6a39b9685
bcaldasz <brian.caldas@morningstar.com>
2021-06-04 17:13:27 -0500

    Added methods delete and set default  portfolio

--- routes/api/v1/portfolio-controller.js
+++ routes/api/v1/portfolio-controller.js
@@ -14,9 +14,16 @@ export const getAllPortfoliosRoute = async (req, res) => {
   res.status(200).json(successResponse(portfolioResponse.allPortfolios));
 };
 
-
 export const getMyPortfoliosRoute = async (req, res) => {
   res.status(200).json(successResponse(portfolioResponse.myPortfolios));
+};
+
+export const setDefaultPortfolio = async (req, res) => {
+  res.status(200).json(successResponse());
+};
+
+export const deletePortfolio = async (req, res) => {
+  res.status(200).json(successResponse());
 };
 
 controllerRouter.use(authorizationHeader);
@@ -25,6 +32,10 @@ controllerRouter.use(authorizationHeader);
 controllerRouter.get('/all', getAllPortfoliosRoute);
 /** @path /portfolios/my-portfolios */
 controllerRouter.get('/my-portfolios', getMyPortfoliosRoute);
+/** @path /portfolios/set-portfolio-default */
+controllerRouter.post('/set-portfolio-default', setDefaultPortfolio);
+/** @path /portfolios/delete-portfolio */
+controllerRouter.delete('/delete-portfolio', deletePortfolio);
 
 // create and export default register controller function
 const registerClientConfigController = registerControllerGenerator(controllerRouter);
