ea44678dd175b21b990a6e7370bfa9e7d74932f2
bcaldasz <brian.caldas@morningstar.com>
2021-06-28 11:44:23 -0500

    Added testing endpoint

--- routes/api/v1/tranches-controller.js
+++ routes/api/v1/tranches-controller.js
@@ -19,12 +19,22 @@ export const selectedTranches= async (req, res) => {
   res.status(200).json(successResponse(selectedTranches));
 };
 
+export const selectedTranches= async (req, res) => {
+  const selectedTranches = tranchesResponse.tranches.slice(1, 8);
+  setTimeout(() => {
+    res.status(200).json(successResponse(selectedTranches));
+  }, 10000);
+};
+
 controllerRouter.use(authorizationHeader);
 
 /** @path /tranches */
 controllerRouter.get('/', getTranches);
 /** @path /tranches/selected-tranches */
 controllerRouter.get('/selected-tranches', selectedTranches);
+/** @path /tranches/selected-tranches-test-time */
+/** @description Endpoint to test */
+controllerRouter.get('/selected-tranches-test-time', selectedTranchesTime);
 
 // create and export default register controller function
 const registerClientConfigController = registerControllerGenerator(controllerRouter);
