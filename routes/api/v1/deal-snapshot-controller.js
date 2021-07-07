1c61beae93629051de798094c0b0117c12d774e2
bcaldasz <brian.caldas@morningstar.com>
2021-07-07 11:46:02 -0500

    Changed deal snapshot controller

--- routes/api/v1/deal-snapshot-controller.js
+++ routes/api/v1/deal-snapshot-controller.js
@@ -5,19 +5,28 @@ import { registerControllerGenerator } from '../../../util/controller-helpers';
 import { successResponse } from '../../../util/default-response';
 import { authorizationHeader } from '../../middleware/authorization-headers';
 
-import dealSnapshotResponse from '../../../mocked-data/deal-snapshot--deal-summary.json';
+import dealSnapshotSummaryResponse from '../../../mocked-data/deal-snapshot--deal-summary.json';
+
+import dealSnapshotResponse from '../../../mocked-data/dealSnapshot.json';
 
 // create router
 export const controllerRouter = express.Router();
 
 export const getDealSummary= async (req, res) => {
-  res.status(200).json(successResponse(dealSnapshotResponse));
+  res.status(200).json(successResponse(dealSnapshotSummaryResponse));
+};
+
+export const getDealSnapshotInfo = async (req, res) => {
+  res.status(200).json(successResponse(dealSnapshotResponse.dealSnapshotInfo));
 };
 
 controllerRouter.use(authorizationHeader);
 
 /** @path /deal-snapshot/:deal/deal-summary */
 controllerRouter.get('/:deal/deal-summary', getDealSummary);
+
+/** @path /deal-snapshot/:deal */
+controllerRouter.get('/:deal/', getDealSnapshotInfo);
 
 // create and export default register controller function
 export default registerControllerGenerator(controllerRouter);
--- routes/api/v1/deals-controller.js
+++ routes/api/v1/deals-controller.js
@@ -6,7 +6,6 @@ import { successResponse } from '../../../util/default-response';
 import { authorizationHeader } from '../../middleware/authorization-headers';
 
 import dealsResponse from '../../../mocked-data/deals.json';
-import dealSnapshotResponse from '../../../mocked-data/dealSnapshot.json';
 
 // create router
 export const controllerRouter = express.Router();
@@ -15,12 +14,8 @@ export const getDeals= async (req, res) => {
   res.status(200).json(successResponse(dealsResponse.deals));
 };
 
-export const getDealSnapshotInfo = async (req, res) => {
-  res.status(200).json(successResponse(dealSnapshotResponse.dealSnapshotInfo));
-};
-
-export const getDealsIdSnapshot = async (req, res) => {
-  const dealsIDs = dealsResponse.deals.map((item) => item.id);
+export const getDealsIds = async (req, res) => {
+  const dealsIDs = dealsResponse.deals.map((item) => ({id: item.id, text: item.id}));
   res.status(200).json(successResponse(dealsIDs));
 };
 
@@ -29,11 +24,8 @@ controllerRouter.use(authorizationHeader);
 /** @path /deals */
 controllerRouter.get('/', getDeals);
 
-/** @path /deals/snapshot-info */
-controllerRouter.get('/snapshot-info', getDealSnapshotInfo);
-
-/** @path /deals/deal-snapshot */
-controllerRouter.get('/deal-snapshot', getDealsIdSnapshot);
+/** @path /deals/deal-ids */
+controllerRouter.get('/deal-ids', getDealsIds);
 
 // create and export default register controller function
 const registerClientConfigController = registerControllerGenerator(controllerRouter);
