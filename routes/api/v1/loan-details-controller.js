107cb6cd5d7c78398476d78fbd8f053a40647436
Jose Erazo <jerazo@US-C02FP4ZEMD6P>
2021-10-21 16:09:02 -0500

    feat: Added mock data and controller for loan details, MCIA-522

---
+++ routes/api/v1/loan-details-controller.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import get from 'lodash/get';
+import config from '../../../properties/config';
+import { registerControllerGenerator } from '../../../util/controller-helpers';
+import { successResponse } from '../../../util/default-response';
+import { authorizationHeader } from '../../middleware/authorization-headers';
+
+import loanDetailsLoanDataResponse from '../../../mocked-data/loan-details-loan-data.json';
+
+// create router
+export const controllerRouter = express.Router();
+
+export const getLoanData = async (req, res) => {
+  res.status(200).json(successResponse(loanDetailsLoanDataResponse));
+};
+
+controllerRouter.use(authorizationHeader);
+
+/** @path /loan-details/:deal/loan-data */
+controllerRouter.get('/:deal/loan-data', getLoanData);
+
+// create and export default register controller function
+export default registerControllerGenerator(controllerRouter);
