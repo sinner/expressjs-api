90ceaa04108869f319d27ced879c4abe76f52ec1
Jose Erazo <jerazo@US-C02FP4ZEMD6P>
2021-11-03 15:57:08 -0500

    chore: Added route for financials tab, MCIA-542

--- routes/api/v1/loan-details-controller.js
+++ routes/api/v1/loan-details-controller.js
@@ -6,6 +6,7 @@ import { successResponse } from '../../../util/default-response';
 import { authorizationHeader } from '../../middleware/authorization-headers';
 
 import loanDetailsLoanDataResponse from '../../../mocked-data/loan-details-loan-data.json';
+import loanDetailsFinancialResponse from '../../../mocked-data/loan-details-financials-data.json';
 
 // create router
 export const controllerRouter = express.Router();
@@ -13,11 +14,16 @@ export const controllerRouter = express.Router();
 export const getLoanData = async (req, res) => {
   res.status(200).json(successResponse(loanDetailsLoanDataResponse));
 };
+export const getFinancial = async (req, res) => {
+  res.status(200).json(successResponse(loanDetailsFinancialResponse));
+};
 
 controllerRouter.use(authorizationHeader);
 
 /** @path /loan-details/:deal/loan-data */
 controllerRouter.get('/:deal/loan-data', getLoanData);
+/** @path /loan-details/:deal/financials */
+controllerRouter.get('/:deal/financial', getFinancial);
 
 // create and export default register controller function
 export default registerControllerGenerator(controllerRouter);
