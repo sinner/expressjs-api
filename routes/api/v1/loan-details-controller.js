2e9d5739369992a5fe5ff94e8ed8a3dea82c5f89
Jose Erazo <jose.erazo@morningstar.com>
2021-12-23 20:47:40 -0500

    chore: Added new endpoints for pirvate notes and shared notes, MCIA-643

--- routes/api/v1/loan-details-controller.js
+++ routes/api/v1/loan-details-controller.js
@@ -8,6 +8,8 @@ import { authorizationHeader } from '../../middleware/authorization-headers';
 import loanDetailsLoanDataResponse from '../../../mocked-data/loan-details-loan-data.json';
 import loanDetailsFinancialResponse from '../../../mocked-data/loan-details-financials-data.json';
 import sharedDocmentResponse from '../../../mocked-data/shared-documents.json';
+import privateNotesResponse from '../../../mocked-data/private-notes.json';
+import sharedNotesResponse from '../../../mocked-data/shared-notes.json';
 
 // create router
 export const controllerRouter = express.Router();
@@ -24,7 +26,24 @@ export const getSharedDocuments = async (req, res) => {
 export const uploadSharedDocuments = async (req, res) => {
   res.status(200).json(successResponse(sharedDocmentResponse));
 };
-
+export const getPrivateNotes = async (req, res) => {
+  res.status(200).json(successResponse(privateNotesResponse));
+};
+export const getSharedNotes = async (req, res) => {
+  res.status(200).json(successResponse(sharedNotesResponse));
+};
+export const addPrivateNote = async (req, res) => {
+  res.status(200).json(successResponse());
+};
+export const deleteSharedNote = async (req, res) => {
+  res.status(200).json(successResponse());
+};
+export const editSharedNote = async (req, res) => {
+  res.status(200).json(successResponse());
+};
+export const addSharedNote = async (req, res) => {
+  res.status(200).json(successResponse());
+};
 
 controllerRouter.use(authorizationHeader);
 
@@ -36,6 +55,16 @@ controllerRouter.get('/:loan/financial', getFinancial);
 controllerRouter.get('/:loan/shared-documents', getSharedDocuments);
 /** @path /loan-details/:loan/shared-documents */
 controllerRouter.post('/upload-documents', uploadSharedDocuments);
+/** @path /loan-details/:loan/private-notes */
+controllerRouter.get('/:loan/private-notes', getPrivateNotes);
+/** @path /loan-details/:loan/shared-notes */
+controllerRouter.get('/:loan/shared-notes', getSharedNotes);
+/** @path /loan-details/:loan/edit-shared-notes */
+controllerRouter.get('/:loan/edit-shared-notes', editSharedNote);
+/** @path /loan-details/:loan/delete-shared-notes */
+controllerRouter.get('/:loan/delete-shared-notes', deleteSharedNote);
+/** @path /loan-details/:loan/add-private-notes */
+controllerRouter.get('/:loan/add-private-notes', addPrivateNote);
 
 
 // create and export default register controller function
